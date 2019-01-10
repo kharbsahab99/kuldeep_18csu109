9a17fa300db49bdba7e9190eb560cfa7d99cfcc5
kharbsahab99 <46473377+kharbsahab99@users.noreply.github.com>
2019-01-10 08:51:24 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Hello World");
+	
+	}
+}
