04701c3a2ebb9d40713945875ba58b9b5d9485b4
kharbsahab99 <46473377+kharbsahab99@users.noreply.github.com>
2019-01-21 10:00:39 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Hello World");
-	
-	}
-}
