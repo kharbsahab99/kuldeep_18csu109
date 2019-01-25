0ec540a06f74bb4ee19d2bb7051d47265e00f37a
kharbsahab99 <46473377+kharbsahab99@users.noreply.github.com>
2019-01-25 10:02:41 +0530

    Delete helloworld.java

--- FOCP2/datatypes/src/helloworld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class helloworld {
-	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Hello World");
-	
-	}
-}
